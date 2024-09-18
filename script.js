494f64d43a796049f84660c97aaf527a030c489e
lanewells <wellsdelaneyk@gmail.com>
2024-09-18 13:18:52 -0700

    complete and submitting in

--- script.js
+++ script.js
@@ -4,12 +4,19 @@
  * Declare a variable called myFavoriteSong on one line and console.log the value
  * Then, using your Literals, console.log "my favorite song is..." with the song's value
  */
+let myFavoriteSong = 'Tadow'
 
+console.log(`my favorite song is ${myFavoriteSong}`)
 /*
  * Prompt 2:
  *
  * Create 3 variables using let and const, then console.log each of them, using a string literal (` ${} `} in the log
  */
+let theWeather = 'sunny'
+let theTemp = 65
+const location = 'San Francisco'
+
+console.log(`Today in ${location}, the weather is ${theWeather} and ${theTemp} degrees Fahrenheit`)
 
 /*
  * Prompt 3:
@@ -21,7 +28,13 @@
  *   - divide your variable by 7
  *   - calculate the remainder of dividing your variable by 2
  */
+let crazyScale = 8
 
+console.log(crazyScale + 3)
+console.log(crazyScale + 1.1)
+console.log(crazyScale * 3)
+console.log((crazyScale / 7).toFixed(2))
+console.log(crazyScale % 2)
 /*
  * Prompt 4:
  *
@@ -33,29 +46,48 @@
  * and lastName variables.
  * Run a console.log that says 'hello my name is..." with your fullName. 
  */
+const firstName = "Lane"
+const lastName = "Wells"
+const fullName = (firstName + " " + lastName)
 
+console.log(`hello my name is ${fullName}`)
 
 /* Arrays */
 
 // Create an array of Movies, then log the 2nd movie in your array
 // Use your array methods to add a movie in to the Front of the array, then the Back of the Array, then to the 2nd index point of the array
+let movies = ['Iron Giant', 'Frozen II', 'Little Miss Sunshine', 'Barbie']
 
+console.log(movies[1])
 
+movies.unshift('Mean Girls')
+movies.push('Up')
+movies.splice(2, 0, "Frozen I")
+
+console.log(movies)
 /*
  *
  * Retrieve "Marty McFly" from each of the arrays below.
  */
 
 let array1 = ['Marty Marion', 'Marty Feldman', 'Marty McFly', 'Marty Marion'];
+
+console.log(array1[2])
+
 let array2 = [
   ['Marty Feldman', 'Marty Marion'],
   ['Marty Stuart', 'Marty McFly'],
   ['Marty Jannetty', 'Marty Robbins']
 ];
+
+console.log(array2[1][1])
+
 let array3 = [
   ['Marty Feldman', ['Marty Marion']],
   ['Marty Stuart', ['Marty Janetty', ['Marty McFly'], 'Marty Robbins']]
 ];
+
+console.log(array3[1][1][1][0])
 
 /*
  *
@@ -63,6 +95,8 @@ let array3 = [
  */
 
 let array4 = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(array4[array4.length - 2])
 
 /*
  *
@@ -76,10 +110,22 @@ let cathleen = ['Cathleen', 186, 'New York'];
 
 // Cathleen decides that Thom can't be named "Thom" anymore. Remove "Thom" from
 // the thom array and replace it with "Tom".
+thom[0] = 'Tom'
+
+console.log(thom)
 
 // Karolin just had her birthday; change Karolin's array to reflect her being
 // a year older.
+karolin[1] = (karolin[1] + 1)
+
+console.log(karolin)
 
 // Change Cathleen's hometown from New York to "Gotham City".
+cathleen.pop()
+cathleen.push('Gotham City')
+console.log(cathleen)
 
 // Remove "Pittsburgh" from Kristyn's array and add "Oakland".
+kristyn.pop()
+kristyn.push('Oakland')
+console.log(kristyn)
